01da691b86b9c2b4602c15ad47cf614b153c4203
Noêmi Ribeiro <noemiribeiro@gmail.com>
2025-09-15 12:16:30 -0300

    TP 01
    Resolução de Trabalho Prático 01 da disciplina de Inteligência Artificial - calibração de pesos, bias e função degrau para criação de neurônios MCP para diferentes funções lógicas booleanas.

---
+++ neurônio MCP OR.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Editor Spyder
+
+"""
+
+from numpy import dot
+
+def função_degrau(x):
+    return 1 if x >= 0.5 else 0
+
+def neurônio_MCP(weights, bias, x):
+    vk = dot(weights, x) + bias
+    return função_degrau(vk)
+
+x0 = [0,0]
+x1 = [0,1]
+x2 = [1,0]
+x3 = [1,1]
+
+weights = [1,1]
+bias = 0
+
+saida0 = neurônio_MCP(weights, bias, x0)
+saida1 = neurônio_MCP(weights, bias, x1)
+saida2 = neurônio_MCP(weights, bias, x2)
+saida3 = neurônio_MCP(weights, bias, x3)
+
+print("NEURÔNIO MCP IMPLEMENTANDO FUNÇÃO BOOLEANA OR")
+print ("0 OR 0 = ", saida0)
+print ("0 OR 1 = ", saida1)
+print ("1 OR 0 = ", saida2)
+print ("1 OR 1 = ", saida3)
